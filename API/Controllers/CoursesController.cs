89d083d47525f5dd329d5b6bc30551aef04fc42a
atligudmunds <a13@ru.is>
2016-09-07 19:34:36 +0000

    first commit

---
+++ API/Controllers/CoursesController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using TestCoursesAPI.Models;
+using TestCoursesAPI.Services;
+
+namespace TestCoursesAPI.API.Controllers
+{
+    [Route("api/courses")]
+    public class CoursesController : Controller
+    {
+        private readonly ICoursesService _service;
+
+        public CoursesController(ICoursesService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public List<CourseLiteDTO> GetCoursesOnSemester(string semester = null)
+        {
+            if(semester == null)
+            {
+                semester = "20163";
+            }
+
+            return _service.GetCourseBySemester(semester);
+            
+            /*return new List<CourseLiteDTO>
+            {
+                new CourseLiteDTO
+                {
+                    ID = 1,
+                    Name = "Web Services",
+                    Semester = "20163"
+                }
+            };*/
+        }
+    }
+}
---
+++ Services/CoursesService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using TestCoursesAPI.Models;
+
+namespace TestCoursesAPI.Services
+{
+    public class CoursesService : ICoursesService
+    {
+        private readonly AppDataContext _db;
+        public CoursesService(AppDataContext db)
+        {
+            _db = db;
+        }
+
+        public List<CourseLiteDTO> GetCourseBySemester(string semester)
+        {
+            return (from x in _db.Courses
+                    where x.Semester == semester
+                    select new CourseLiteDTO
+                    {
+                        ID = x.ID,
+                        Name = x.Name,
+                        Semester = x.Semester
+                    }).ToList();
+        }
+        // TODO: add more functions!
+    }
+}
